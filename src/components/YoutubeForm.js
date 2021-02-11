1d3d035fdee0e20a305be3495e846d368ca52cb1
Street Coding <oldmanstreetcoding@gmail.com>
2021-02-11 14:46:26 +0700

    handling form submission

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -3,6 +3,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+
 // 1. import useFormik
 import { useFormik } from 'formik';
 
@@ -14,22 +15,30 @@ const YoutubeForm = () => {
       email: '',
       channel: '',
     },
+    // 5. add onSubmit function
+    onSubmit: (values) => {
+      console.log('Form Data', values);
+    },
   });
 
   return (
     <div>
-      <label htmlFor="name">Name</label>
-      {/* 3. add props onChange and value in ech form type */}
-      <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+      {/* 4. add props onSubmit */}
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        {/* 3. add props onChange and value in ech form type */}
+        <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
 
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
 
-      <label htmlFor="channel">Channel</label>
-      <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
 
-      <button>Submit</button>
+        <button type="submit">Submit</button>
+      </form>
     </div>
+
   );
 };
 
